34fd47ac5639a7b1680db762ec70c7525809e604
iman esmaeeli <imanmobiledev@gmail.com>
2024-06-04 01:56:22 +0330

    feat:add authentication email

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,11 +7,13 @@ use App\Enums\StatusCode;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Resources\Auth\RegisterResource;
+use App\Mail\auth\UserCreated;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 //use App\Utils\Response\Response;
 use App\Facades\Response;
+use Illuminate\Support\Facades\Mail;
 
 class RegisterController extends Controller
 {
@@ -21,6 +23,8 @@ class RegisterController extends Controller
     public function __invoke(RegisterRequest $request)
     {
         $user=User::create($request->toArray());
+
+        Mail::to($user->email)->send(new UserCreated());
 
         return Response::success(
             trans('auth.register_success'),
