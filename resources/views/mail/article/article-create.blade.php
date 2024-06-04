8961062297b5c15bab2be126507cb055f47385fc
iman esmaeeli <imanmobiledev@gmail.com>
2024-06-04 14:34:29 +0330

    feat:add email sender for create article

---
+++ resources/views/mail/article/article-create.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Article activity!</title>
+</head>
+<body>
+    <h3>Hi your article with title: {{$title}} created.</h3>
+    <p>visibility of its is {{$status}}</p>
+</body>
+</html>
