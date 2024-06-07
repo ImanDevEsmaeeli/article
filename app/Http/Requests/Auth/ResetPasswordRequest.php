90270949dae56e5c8f2a157cc9e192f10b1bbe47
iman esmaeeli <imanmobiledev@gmail.com>
2024-06-07 20:33:27 +0330

    complete reset password algorithm

--- app/Http/Requests/Auth/ResetPasswordRequest.php
+++ app/Http/Requests/Auth/ResetPasswordRequest.php
@@ -1,28 +1,30 @@
 <?php
 
-namespace App\Http\Requests\auth;
+    namespace App\Http\Requests\Auth;
 
-use Illuminate\Foundation\Http\FormRequest;
+    use Illuminate\Foundation\Http\FormRequest;
 
-class ResetPasswordRequest extends FormRequest
-{
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
+    class ResetPasswordRequest extends FormRequest
     {
-        return true;
-    }
+        /**
+         * Determine if the user is authorized to make this request.
+         */
+        public function authorize(): bool
+        {
+            return true;
+        }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
-    {
-        return [
-            'email'=>'required|email|exists:users,email',
-        ];
+        /**
+         * Get the validation rules that apply to the request.
+         *
+         * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+         */
+        public function rules(): array
+        {
+            return [
+                'token' => 'required',
+                'email' => 'required|email|exists:users,email',
+                'password' => 'required|min:8|confirmed',
+            ];
+        }
     }
-}
